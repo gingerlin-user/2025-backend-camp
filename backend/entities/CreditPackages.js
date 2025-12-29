92cf7313ea86539298cd04e03dd23c5677ee5fa9
gonsakon <gonsakon@gmail.com>
2025-12-29 12:45:28 +0800

    feat: 初始化專案

---
+++ backend/entities/CreditPackages.js
@@ -0,0 +1,45 @@
+const { EntitySchema } = require('typeorm')
+/**
+ * 堂數方案資料表
+ *
+ * 欄位說明：
+ * - id: 方案編號（UUID）
+ * - name: 方案名稱
+ * - credit_amount: 包含堂數
+ * - price: 方案價格
+ * - createdAt: 建立時間
+ */
+module.exports = new EntitySchema({
+  name: 'CreditPackage',
+  tableName: 'CREDIT_PACKAGE',
+  columns: {
+    id: {
+      primary: true,
+      type: 'uuid',
+      generated: 'uuid',
+      nullable: false
+    },
+    name: {
+      type: 'varchar',
+      length: 50,
+      nullable: false,
+      unique: true
+    },
+    credit_amount: {
+      type: 'integer',
+      nullable: false
+    },
+    price: {
+      type: 'numeric',
+      precision: 10,
+      scale: 2,
+      nullable: false
+    },
+    createdAt: {
+      type: 'timestamp',
+      createDate: true,
+      name: 'created_at',
+      nullable: false
+    }
+  }
+})
